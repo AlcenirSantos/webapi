e708324f88afc4d35eb138111be3e3cf2c89c504
Alcenir Barbosa dos Santos <alcenir.santos@atualsistemas.com.br>
2022-02-09 11:03:46 -0300

    teste api

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> listValues()
+        {
+            List<string> lista = new List<string>();
+            lista.Add("carro");
+            lista.Add("Moto");
+            lista.Add("Bicicleta");
+            return lista;
+        }
+    }
+}
